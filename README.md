520611ef09525e34ba4cb9efcd16072a7876b336
Pedro Nasser <pedronasser@gmail.com>
2012-09-02 23:46:23 -0300

    changing readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-WebNode Server 
+MWebNode Server 
 ========
 
 A NodeJS MVC Framework and HTTP Server. Based on the structure of the world's best PHP Framework: [Yii](http://yiiframework.com).
@@ -9,16 +9,29 @@ A NodeJS MVC Framework and HTTP Server. Based on the structure of the world's be
 
 ### @ Requirement
 
-todo
+ - NodeJS v0.8.8 ([how to install](https://github.com/joyent/nodejs))
 
 ### @ NPM mode
 
-todo
+ This mode requires to install the last version of NPM.
+
+   $ npm install https://github.com/pedronasser/wnServer
 
 ### @ Manual mode
 
-todo
+ This mode requires the installation of `git` or just download the source.
 
-## Quick start new project
+## Quick start and play
 
-todo
+ The first step is to create a new application:
+ 
+ - Go to the folder where you downloaded the source of webNode
+ 
+   $ node index.js newapp [domain/ip] ##[path to the project]
+
+   Example:
+   
+   $ node index.js newapp 127.0.0.1 app/helloworld
+   
+   This will automatictally create a new application from the default inside that directory and bind the new application to the webNode loading configuration.
+
--- index.js
+++ index.js
@@ -1,54 +1,18 @@
-/*#
-  * @WebNode - A NodeJS-MVC HTTP Server.
-  * http://pedroncs.com/projects/webnode/
-  * 
-  * All rights reserved.
-  */
+/**
+ * @WebNode - A NodeJS MVC Framework and HTTP Server
+ * 
+ * @author: Pedro Nasser
+ * @link: http://pedroncs.com/projects/webnode/
+ * @license: http://pedroncs.com/projects/webnode/#license
+ * @copyright: Copyright &copy; 2012 WebNode Server
+ */
 
-console.log('Starting server..');
-	sourcePath = 'source/'; // Caminho para a source.
-	cwd = process.cwd()+'/'; // Caminho atual.
-	_r = require; // Alias para require
+/**
+ * Calls the wnInit.
+ *
+ * @author Pedro Nasser
+ * @version $Id$
+ * @since 1.0.0
+ */
 
-// Carregando modulos do NODEJS.
-console.log('Loading required node modules..');
-	http = _r('http');
-	fs = _r('fs');
-	url = _r('url');
-	zlib = _r('zlib');
-	stream = _r('stream');
-	util = _r('util');
-	emitter = require('events').EventEmitter;
-	Buffer = _r('buffer').Buffer;
-
-	// Carrega as classes padrões...
-	console.log('Loading required core classes...');
-	var classes=fs.readdirSync(cwd+sourcePath+'core/');
-	for (c in classes) {
-		console.log(' - Loaded class: /'+sourcePath+'core/'+classes[c]);
-		global[classes[c].split('.')[0]] = _r(cwd+sourcePath+'core/'+classes[c]);
-	}
-
-	// Carrega as libraries padrões...
-	console.log('Loading required libraries classes...');
-	var classes=fs.readdirSync(cwd+sourcePath+'lib/');
-	for (c in classes) {
-		console.log(' - Loaded library: /'+sourcePath+'lib/'+classes[c]);
-		global[classes[c].split('.')[0]] = _r(cwd+sourcePath+'lib/'+classes[c]);
-	}
-
-	// Erros não acolhidos...
-	process.on('uncaughtException', function (e) { new wnError(e); });
-
-	/* Informações estáticas desta versão. */
-	/* Não mudar */
-	wnServer.prototype.version = '1.0.0';
-	wnServer.prototype.servername = 'webNode';
-	wnServer.prototype.author = 'Pedro Nasser';
-
-(function () {
-
-	// Cria um novo WebNode Server.
-	var web = new wnServer;
-
-})();
+require('./source/wnInit.js');
