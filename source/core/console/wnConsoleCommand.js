58a2102a99069e0800d2cc1dfa269f08d0222907
Pedro Nasser <pedronasser@gmail.com>
2012-09-03 14:59:30 -0300

    some loading changes

---
+++ source/core/console/wnConsoleCommand.js
@@ -0,0 +1,37 @@
+/**
+ * Source of the wnConsoleCommand class.
+ * 
+ * @author: Pedro Nasser
+ * @link: http://pedroncs.com/projects/webnode/
+ * @license: http://pedroncs.com/projects/webnode/#license
+ * @copyright: Copyright &copy; 2012 WebNode Server
+ */
+
+/**
+ * {full_description}
+ *
+ * @author Pedro Nasser
+ * @version $Id$
+ * @pagackge system.base
+ * @since 1.0.0
+ */
+
+// Exports.
+module.exports = wnConsole;
+	
+/**
+ * Constructor
+ * {description}
+ */	
+function wnConsole(args) {
+
+ }
+
+/**
+ * @var object the configuration of the server.
+ */
+wnConsole.prototype.config = {};
+
+wnConsole.prototype.exec = function (args) {
+	console.log(args);
+};
